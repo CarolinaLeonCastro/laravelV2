3ab1db50a6238d186d4c790ab5029b340178f53b
Carolina Leon <leoncarolina35@gmail.com>
2025-02-27 16:53:44 +0100

    wip

--- _app.blade.php
+++ _app.blade.php
@@ -4,6 +4,7 @@
 		<title>Jeux</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
 		@vite(['resources/css/app.css', 'resources/js/app.js'])
 	</head>
 	<body>
